6d0f05b4173a523f5101cdefd6c2917fd50f20dd
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-09-30 22:22:40 +0200

    Implement my_self method

--- Enumerable.rb
+++ Enumerable.rb
@@ -1,3 +1,10 @@
 module Enumerable
-    
+    def self.my_each(array)
+        for value in array do
+            puts "Hello, " + value
+        end
+    end
+
+    # Print #my_each
+    my_each(["Jeph", "Anna", "Jack", "Eva"])
 end
