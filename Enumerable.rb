11fedc78c9f8768978c013c3987954d6456c6f7f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-10-01 19:13:26 +0200

    Implement my_each method using yield

--- Enumerable.rb
+++ Enumerable.rb
@@ -1,10 +1,17 @@
 module Enumerable
-    def self.my_each(array)
-        for value in array do
-            puts "Hello, " + value
+
+    #Create #my_each, a method that is identical to #each but (obviously) does not use #each. 
+    #You'll need to remember the yield statement. Make sure it returns the same thing as #each as well.
+
+    def my_each
+        for value in self do
+            yield value
         end
     end
 
-    # Print #my_each
-    my_each(["Jeph", "Anna", "Jack", "Eva"])
+    
+
+    #["Jeph", "Anna", "Jack", "Eva"].each { |friend| puts "Hello, " + friend }
+    ["Jeph", "Anna", "Jack", "Eva"].my_each { |friend| puts "Helloo, " + friend }
 end
+
